46c8ca9221aa93872b1628da392188094e9a1d3d
HoussemEL <houssem.eleuch@esprit.tn>
2021-06-27 13:08:33 +0100

    fixdeleteDoctor

--- assets/js/index.js
+++ assets/js/index.js
@@ -73,3 +73,23 @@ if(window.location.pathname == "/usersl"){
 
     })
 }
+
+if(window.location.pathname == "/list-doctors"){
+    $ondelete = $(".table tbody td a.delete");
+    $ondelete.click(function(){
+        var id = $(this).attr("data-id")
+
+        var request = {
+            "url" : `http://localhost:3000/api/doctors/${id}`,
+            "method" : "DELETE"
+        }
+
+        if(confirm("Do you really want to delete this record?")){
+            $.ajax(request).done(function(response){
+                alert("Data Deleted Successfully!");
+                location.reload();
+            })
+        }
+
+    })
+}

3159a79e028d6ee712e97725a398a26d1ab01172
achrafxor <achrafsaoud@outlook.com>
2021-07-02 00:48:41 +0100

    ezdz

--- custom.py
+++ custom.py
@@ -1,12 +1,23 @@
 
-#from ansible.module_utils.basic import *
-
+from ansible.module_utils.basic import *
 import difflib
 import sys
 import re
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 def checkdiffInValues(d1,d2):
-  print("function")
+  print(bcolors.WARNING + "some values are different between two files" + bcolors.ENDC)
+
   obj1={}
   obj2={}
   indexed=False
@@ -24,7 +35,9 @@ def checkdiffInValues(d1,d2):
        obj2={}
      
   for item in difference['file1']: 
+    # print(bcolors.FAIL)
      print(item)
+    # print(bcolors.ENDC)
   
   print(difference)     
 
@@ -37,8 +50,8 @@ def checkdiffInValues(d1,d2):
 def checkDifferenceInKeys(d1,d2):
     difference={"file1":[],"file2":[]}
     exist=False
-    print ("function")
-    print (difference)
+    print(bcolors.WARNING + "some variables are missing between two files" + bcolors.ENDC)
+    
     #difference["file1"].append("x")
 
     #loop and difference that exist in file1 and dosent exist in file2
@@ -61,28 +74,39 @@ def checkDifferenceInKeys(d1,d2):
      exist=False
 #end loop
 
-    print(difference)
+    #print(bcolors.FAIL)
     print("exist only in file1")
     print("----------------------------------------------------------")
     for value in difference["file1"]:
-        print (value)
+        print ("+ "+ value)
     
     print("----------------------------------------------------------")
 
     print("exist only in file2")
     print("----------------------------------------------------------")
     for value in difference["file2"]:
-        print (value)
+        print ("+ "+ value)
       
     print("----------------------------------------------------------")
           
+    #print(bcolors.ENDC)
  
     
 
 
 
 def main():
+    
     #regex x=re.search("^x.*:$",text)
+ module = AnsibleModule( 
+    argument_spec=dict( 
+        file1 =dict(required=True, type='str'),
+        file2 =dict(required=True, type='str'),
+
+    ) 
+ )
+ file1= module.params.get('file1')
+ file2= module.params.get('file2')
  obj= {}
  file1dict={}
  file2dict={}
@@ -90,17 +114,17 @@ def main():
  file2Plus = []
  existingOnlyInFile1=[]
  existingOnlyInFile2=[]
- file_1 = sys.argv[1]
- file_2 = sys.argv[2]
+ #file_1 = sys.argv[1]
+ #file_2 = sys.argv[2]
  #module = AnsibleModule(
  #   argument_spec=dict(
  #      file1 = dict(required=True, type='str'),
  #      file2 = dict(required=True, type='str'),
  #   )
  #)
- with open(file_1) as f1:
+ with open(file1) as f1:
   f1_reader=f1.readlines()
- with open(file_2) as f2:
+ with open(file2) as f2:
   f2_reader=f2.readlines()
   
  for x in f1_reader:
@@ -113,7 +137,7 @@ def main():
   file2dict.update(obj)
 
  
- #checkDifferenceInKeys(file1dict,file2dict)
+ checkDifferenceInKeys(file1dict,file2dict)
  checkdiffInValues(file1dict,file2dict)
 
 if __name__== '__main__':
